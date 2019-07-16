ef16ef83525e8dbb335bdad6aea8d30923ad6a65
AKP <mgakpmail@gmail.com>
2019-07-16 18:55:23 +0630

    tidy file structure & implement sidebar

---
+++ sites/res/js/sidebar.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+    var collapsibleElem = document.querySelector('.collapsible');
+    var collapsibleInstance = M.Collapsible.init(collapsibleElem);
+});
