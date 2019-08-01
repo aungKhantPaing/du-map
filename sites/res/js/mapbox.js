9b96545becf772b29b429db617639a36fd45e6da
aungkhantpaing <mgakpmail@gmail.com>
2019-08-01 23:17:37 +0630

    updateMap

--- sites/res/js/mapbox.js
+++ sites/res/js/mapbox.js
@@ -6,6 +6,8 @@ var map = new mapboxgl.Map({
     zoom: 14.8,
     // bearing: -27.5 // rotation
 });
+map.touchZoomRotate.enable();
+map.touchZoomRotate.enableRotation();
 
 // Add zoom, rotation and location controls to the map.
 var zoomControl = new mapboxgl.NavigationControl({
@@ -18,7 +20,7 @@ var locationControl = new mapboxgl.GeolocateControl({
     trackUserLocation: true,
     fitBoundsOptions: {
         // offset: [0,-5.7], // for desktop
-        offset: [0,-3.5], // for mobiles
+        offset: [0, -3.5], // for mobiles
         zoom: 15
     }
 });
@@ -28,3 +30,20 @@ map.addControl(zoomControl).addControl(locationControl);
 function locate() {
     locationControl.trigger()
 }
+
+// Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
+map.on('click', 'poi-label', function (e) {
+    map.flyTo({
+        center: e.features[0].geometry.coordinates
+    });
+});
+
+// Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
+map.on('mouseenter', 'poi-label', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'poi-label', function () {
+    map.getCanvas().style.cursor = '';
+});
--- sites/res/js/sidebar.js
+++ sites/res/js/sidebar.js
@@ -8,6 +8,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     var tabs = document.querySelectorAll('.tabs');
     var instance = M.Tabs.init(tabs, {
-        swipable: true
     });
 });
