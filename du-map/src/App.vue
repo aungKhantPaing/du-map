85f3fd307175b188f3ca1272f3880243439ffa0b
aungkhantpaing <mgakpmail@gmail.com>
2019-11-08 16:13:47 +0630

    create place models and Map component

--- du-map/src/App.vue
+++ du-map/src/App.vue
@@ -1,93 +1,43 @@
 <template>
   <div id="app">
-    <div class="progress-container" v-if="dataLoaded == false">
-      <div class="progress">
-        <div class="indeterminate"></div>
-      </div>
-    </div>
-
-    <div id="map"></div>
-    // TODO: add side bar
+    <Map></Map>
+    <!-- TODO: add progress bar -->
+    <!-- TODO: add side bar -->
   </div>
 </template>
 
 <script lang="ts">
-import mapboxgl from "mapbox-gl";
+import Map from "@/views/components/Map.vue";
+import store from "./store";
+import { mapState } from "vuex";
 
 export default {
-  data() {
-    return {
-      dataLoaded: false
-    };
+  store,
+  components: {
+    Map
   },
-  mounted() {
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoiYWtwMTAxIiwiYSI6ImNqeGtrbnVwazAxM2Izbm1vOWYwdHQxdjkifQ.gtLMDe9KAEU2rxBvk_vnzw";
-    var map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/akp101/cjxkkxwpc01x11cnur0aepitf/draft",
-      center: [96.212739, 16.911199],
-      zoom: 14.8,
-      maxBounds: [
-        [96.20043008891338, 16.899012663005408], // Southwest coordinates
-        [96.22252355799174, 16.9239222243597] // Northeast coordinates
-      ],
-      bearing: -27.5 // rotation
-    });
-
-    map.touchZoomRotate.enable();
-    map.touchZoomRotate.enableRotation();
-
-    // Add zoom, rotation and location controls to the map.
-    var zoomControl = new mapboxgl.NavigationControl({
-      visualizePitch: true
-    });
-    var locationControl = new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      trackUserLocation: true,
-      fitBoundsOptions: {
-        // offset: [0,-5.7], // for desktop
-        offset: [0, -3.5], // for mobiles
-        zoom: 15
-      }
-    });
-    map.addControl(zoomControl).addControl(locationControl);
-
-    map.on("load", () => {
-      this.dataLoaded = true;
-    });
-  }
+  computed: mapState(["dataLoaded", "placeLists"])
 };
 </script>
 
 <style lang="scss" scoped>
-// put ~ infront to import node_module
 //? can't find a way to import scss yet
-@import url("~materialize-css/dist/css/materialize.css");
+//* put '~' in front of directory to import node_module
 
-#map {
-  overflow: hidden;
-  overflow-x: hidden;
-  overflow-y: hidden;
-  height: 100%;
-  width: 100%;
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  right: 0px;
-  bottom: 0px;
-}
+// @import url("~materialize-css/dist/css/materialize.css");
+/*
+  materialize-css is currently vulnerable to Cross-Site Scripting
+  more detail: https://www.npmjs.com/advisories/817
+*/
 
-.progress-container {
-  height: 100vh;
-  .progress {
-    position: fixed;
-    top: 50vh;
-    .indeterminate {
-      width: 70%;
-    }
-  }
-}
+// .progress-container {
+//   height: 100vh;
+//   .progress {
+//     position: fixed;
+//     top: 50vh;
+//     .indeterminate {
+//       width: 70%;
+//     }
+//   }
+// }
 </style>
---
+++ du-map/src/models/place.ts
@@ -0,0 +1,9 @@
+export default class Place {
+  properties: any;
+  coordinates?: any;
+
+  constructor(properties: any, coordinates: any) {
+    this.properties = properties;
+    this.coordinates = coordinates;
+  }
+}
