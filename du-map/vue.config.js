e4e73cb8a7dfc8099a634387bb8e80778cc15f07
aungkhantpaing <mgakpmail@gmail.com>
2019-11-30 17:34:02 +0630

    add vue.config for debugging in vs code

--- du-map/vue.config.js
+++ du-map/vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ['vuetify'],
+  configureWebpack: {
+    devtool: 'source-map', // for debugging in VS Code (https://vuejs.org/v2/cookbook/debugging-in-vscode.html#Displaying-Source-Code-in-the-Browser)
+  },
 };
