48e70238376708860d9cee7202d26323f4f46c2d
aungKhantPiang <mgakpmail@gmail.com>
2019-12-01 21:03:38 +0630

    add built dist folder

---
+++ du-map/dist/precache-manifest.849b03186e5bf8425e268fc6939277a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7f85cbb8e26a95e4be",
+    "url": "/css/app.0a608feb.css"
+  },
+  {
+    "revision": "1d3de65fab7b509416f1",
+    "url": "/css/chunk-vendors.c2944ba8.css"
+  },
+  {
+    "revision": "5cfb1ed70795659302f8e1f24f3eff1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b7f85cbb8e26a95e4be",
+    "url": "/js/app.7dc47a18.js"
+  },
+  {
+    "revision": "1d3de65fab7b509416f1",
+    "url": "/js/chunk-vendors.b268ff31.js"
+  },
+  {
+    "revision": "6714830901edc6d944253d7ce5629c9d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
