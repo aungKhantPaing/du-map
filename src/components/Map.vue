0063d3bc076a0dab97cd1d9770f086927dc9896e
aungKhantPiang <mgakpmail@gmail.com>
2020-02-04 23:39:06 +0630

    move map's controller a bit below

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,16 +4,16 @@
 
 <script lang="ts">
 /* eslint-disable no-console */
-import { Vue, Component, Prop } from "vue-property-decorator";
-import mapboxgl from "mapbox-gl";
-import { mapMutations } from "vuex";
-import { Place } from "@/models/place";
-import eventBus from "@/eventBus";
-import store from "@/store";
-import { MapboxOptions } from "mapbox-gl/index";
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import mapboxgl from 'mapbox-gl';
+import { mapMutations } from 'vuex';
+import { Place } from '@/models/place';
+import eventBus from '@/eventBus';
+import store from '@/store';
+import { MapboxOptions } from 'mapbox-gl/index';
 
 @Component({
-  methods: mapMutations(["setPlaceList", "setDataLoaded"])
+  methods: mapMutations(['setPlaceList', 'setDataLoaded']),
 })
 export default class Map extends Vue {
   @Prop()
@@ -24,36 +24,36 @@ export default class Map extends Vue {
 
   @Prop({
     type: Boolean,
-    default: () => true
+    default: () => true,
   })
   readonly zoomControl!: Boolean;
 
   @Prop({
     type: Boolean,
-    default: () => false
+    default: () => false,
   })
   readonly locationControl!: Boolean;
 
   options: MapboxOptions = {
-    container: "map",
-    ...this.mapOptions
+    container: 'map',
+    ...this.mapOptions,
   };
 
   config(map: mapboxgl.Map) {
     // Add zoom, rotation and location controls to the map.
     const zoomControl = new mapboxgl.NavigationControl({
-      visualizePitch: true
+      visualizePitch: true,
     });
     const locationControl = new mapboxgl.GeolocateControl({
       positionOptions: {
-        enableHighAccuracy: true
+        enableHighAccuracy: true,
       },
       trackUserLocation: true,
       fitBoundsOptions: {
         // offset: [0,-5.7], // for desktop
         offset: [0, -3.5], // for mobiles
-        zoom: 15
-      }
+        zoom: 15,
+      },
     });
 
     if (this.zoomControl) {
@@ -66,35 +66,37 @@ export default class Map extends Vue {
 
     // 🖱️ set cursor events
     // change the cursor to pointer 👆 when the it enters a feature in the 'symbols' layer.
-    map.on("mouseenter", "poi-label-places", function() {
-      map.getCanvas().style.cursor = "pointer";
+    map.on('mouseenter', 'poi-label-places', function() {
+      map.getCanvas().style.cursor = 'pointer';
     });
     // change it back to a cursor when it leaves.
-    map.on("mouseleave", "poi-label-places", function() {
-      map.getCanvas().style.cursor = "";
+    map.on('mouseleave', 'poi-label-places', function() {
+      map.getCanvas().style.cursor = '';
     });
   }
   // dirty data -> filter -> sort -> clean data
   getCleanedPlaceData(map: mapboxgl.Map): Array<Place> {
     // query dirty list data from map
-    let sourceFeatures = map.querySourceFeatures("composite", {
-      sourceLayer: "DU_Places_New" // require if sourceLayer is a vector_tileset
+    let sourceFeatures = map.querySourceFeatures('composite', {
+      sourceLayer: 'DU_Places_New', // require if sourceLayer is a vector_tileset
     });
 
+    console.log('SOURCE FEATURES');
+    console.log(sourceFeatures);
+
     // filter duplicated data
-    sourceFeatures.forEach(targetPlace => {
+    sourceFeatures.forEach((targetPlace) => {
       var duplicatedItems: any[] = [];
-      sourceFeatures.forEach(place => {
+      sourceFeatures.forEach((place) => {
         if (
-          JSON.stringify(targetPlace.properties) ===
-            JSON.stringify(place.properties) && // compare primitive data
+          JSON.stringify(targetPlace.properties) === JSON.stringify(place.properties) && // compare primitive data
           targetPlace != place // compare reference data
         ) {
           duplicatedItems.push(place); // get the duplicates
         }
       });
       // remove the duplicatedItem(s) of targetPlace
-      duplicatedItems.forEach(item => {
+      duplicatedItems.forEach((item) => {
         var itemIndex = sourceFeatures.indexOf(item);
         sourceFeatures.splice(itemIndex, 1);
       });
@@ -103,8 +105,8 @@ export default class Map extends Vue {
     // sort alphabetically
     sourceFeatures.sort(compare);
     function compare(a: any, b: any) {
-      var aString = "" + a.properties.name_en; // cast to unicode
-      var bString = "" + b.properties.name_en;
+      var aString = '' + a.properties.name; // cast to unicode
+      var bString = '' + b.properties.name;
       if (aString < bString) {
         return -1;
       }
@@ -114,9 +116,7 @@ export default class Map extends Vue {
       return 0;
     }
 
-    console.log(sourceFeatures);
-
-    return sourceFeatures.map(feature => Place.parse(feature));
+    return sourceFeatures.map((feature) => Place.parse(feature));
   }
 
   mounted() {
@@ -125,44 +125,50 @@ export default class Map extends Vue {
     let map = new mapboxgl.Map(this.$data.options);
     this.config(map);
     let marker = new mapboxgl.Marker({
-      color: "#F85A40" // red
+      color: '#F85A40', // red
     })
       .setLngLat([0, 0])
       .addTo(map);
 
     // 👂 LISTEN EVENTs
     // call after map data are fully loaded
-    map.on("load", () => {
-      this.$emit("loaded", this.getCleanedPlaceData(map));
-      eventBus.$emit("loaded");
+    map.on('load', () => {
+      let cleanData = this.getCleanedPlaceData(map);
+      this.$emit('loaded', cleanData);
+
+      console.log('CLEAN DATA');
+      console.log(cleanData);
+      eventBus.$emit('loaded');
     });
 
     // listen to mouse events as soon as the components are mounted.
     // remove highlight when user click/tap other area of the map (non-icon area)
-    map.on("click", _ => {
+    map.on('click', (_) => {
       removeHighlight();
-      this.$router.replace("/");
+      if (this.$router.currentRoute.name != 'home') {
+        this.$router.replace('/');
+      }
     });
     // highlight when user specifically click/tap place icon
     //* event --> router.push --> highlight
-    map.on("click", "poi-label-places", e => {
+    map.on('click', 'poi-label-places', (e) => {
       if (e.features) {
         let place = Place.parse(e.features[0]);
         this.$router.push(`/place/${place.properties.id}`);
       }
     });
 
-    eventBus.$on("removeHighlight", () => {
+    eventBus.$on('removeHighlight', () => {
       removeHighlight();
-      this.$router.replace("/");
+      this.$router.replace('/');
     });
-    eventBus.$on("highlightPlace", (place: Place) => {
+    eventBus.$on('highlightPlace', (place: Place) => {
       highlightPlace(place);
     });
 
     let removeHighlight = () => {
       marker.remove();
-      map.setFilter("building-3d-highlighted", ["in", "id", ""]); // remove highlight
+      map.setFilter('building-3d-highlighted', ['in', 'id', '']); // remove highlight
     };
     let highlightPlace = (place: Place) => {
       marker.remove(); // remove default-marker from mapbox.js
@@ -173,15 +179,11 @@ export default class Map extends Vue {
       }
       map.flyTo({
         center: place.geometry.coordinates,
-        zoom: 18
+        zoom: 18,
       });
 
       // highlight the 3d structure by filtering with equal id
-      map.setFilter("building-3d-highlighted", [
-        "in",
-        "id",
-        place.properties.id
-      ]);
+      map.setFilter('building-3d-highlighted', ['in', 'id', place.properties.id]);
     };
   }
 }
@@ -202,5 +204,11 @@ export default class Map extends Vue {
   top: 0;
   bottom: 0;
   width: 100%;
+}
+</style>
+
+<style>
+.mapboxgl-ctrl-top-right {
+  top: 70px !important;
 }
 </style>
