12a10b17ef040560e4e3f4b4ef6fe6eceb4976d8
aungKhantPaing <mgakpmail@gmail.com>
2020-05-11 20:49:10 +0630

    add meta tags

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,40 @@
 </template>
 
 <script lang="ts">
+let logo = window.origin + require('./assets/logo.png');
 import Vue from 'vue';
 import { App } from '@/models/appState';
 export default Vue.extend({
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      metaInfo: {
+        // if no subcomponents specify a metaInfo.title, this title will be used
+        title: 'DU Map',
+        // all titles will be injected into this template
+        // titleTemplate: '%s | My Awesome Webapp',
+        meta: [
+          // Twitter Card
+          { name: 'twitter:card', content: 'summary' },
+          { name: 'twitter:title', content: 'DU Map' },
+          {
+            name: 'twitter:description',
+            content: 'Find your way in Dagon University.',
+          },
+          // image must be an absolute path
+          { name: 'twitter:image', content: logo },
+          // Facebook OpenGraph
+          { property: 'og:title', content: 'DU Map' },
+          { property: 'og:site_name', content: 'DU Map' },
+          { property: 'og:type', content: 'website' },
+          { property: 'og:image', content: logo },
+          {
+            property: 'og:description',
+            content: 'Find your way in Dagon University.',
+          },
+        ],
+      },
+    };
+  },
   mounted() {
     // if (!window.navigator.onLine) {
     //   this.$router.replace('/offline');
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import router from '@/router';
 import store from '@/store';
 import '@/registerServiceWorker';
 import vuetify from './plugins/vuetify';
+import VueMeta from 'vue-meta';
 
+Vue.use(VueMeta);
 Vue.config.productionTip = false;
 
 //* If your PWA meets the required installation criteria(https://web.dev/install-criteria/),
